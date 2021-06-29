599a9b42e80cd7b352885771c795258f35015753
Ferran Llop <ferranllop@gmail.com>
2021-06-29 13:56:00 +0200

    Throw custom PreconditonError instead of AssertionError

--- src/app/utils/precondition.js
+++ src/app/utils/precondition.js
@@ -1,5 +1,18 @@
 import { strict as assert } from 'assert'
+import { AssertionError } from 'assert'
 
-export function precondition(condition) {
-  assert(condition)
+export function precondition(condition, message) {
+  try {
+    assert(condition, message)
+  } catch (error) {
+    if (error instanceof AssertionError) {
+      throw new PreconditionError(message)
+    } else {
+      throw error
+    }
+  }
+}
+
+export class PreconditionError extends Error {
+
 }
