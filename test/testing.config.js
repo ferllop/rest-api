c2eff578dc97e859495aefc693f7d259950ddaaa
Ferran Llop <ferranllop@gmail.com>
2021-07-08 16:24:03 +0200

    Make Router use precondition instead throwing an error

--- test/testing.config.js
+++ test/testing.config.js
@@ -1,2 +1,13 @@
-export { suite, test } from 'uvu'
-export { strict as assert } from 'assert'
+import { suite } from 'uvu'
+import { strict as assert } from 'assert'
+
+const testingConfig = suite('Testing config')
+
+testingConfig('Preconditions should be enabled when testing', () => {
+    assert(process.env.ENABLE_PRECONDITIONS === "true")
+})
+
+testingConfig.run()
+
+export { test } from 'uvu'
+export { suite, assert }
